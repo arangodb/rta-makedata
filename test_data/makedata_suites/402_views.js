a3d18304505ecc87d0098dfae0a7f5293a5fa490
Alexey Bakharew <alex.bakharew@gmail.com>
2023-04-14 19:23:02 +0200

    Fix merge conflict

--- test_data/makedata_suites/402_views.js
+++ test_data/makedata_suites/402_views.js
@@ -635,17 +635,10 @@ function deleteAnalyzer_400(testgroup, analyzerName){
 
     // remove 'cache' values from link/index definition
     if (result.hasOwnProperty("cache")) {
-<<<<<<< HEAD
       if (has_animal) {
         if (result["cache"] == false) {
           if (animal_field.hasOwnProperty("cache")) {
             if (animal_field["cache"] == false) {
-=======
-      if (result["fields"].hasOwnProperty("animal")) {
-        if (result["cache"] === false) {
-          if (result["fields"]["animal"].hasOwnProperty("cache")) {
-            if (result["fields"]["animal"]["cache"] === false) {
->>>>>>> main
               delete result["cache"];
               delete animal_field["cache"];
             } else {
@@ -655,31 +648,18 @@ function deleteAnalyzer_400(testgroup, analyzerName){
             delete result["cache"];
           }
         } else {
-<<<<<<< HEAD
           if (animal_field.hasOwnProperty("cache")) {
             if (animal_field["cache"] == true) {
               delete animal_field["cache"];
-=======
-          if (result["fields"]["animal"].hasOwnProperty("cache")) {
-            if (result["fields"]["animal"]["cache"] === true) {
-              delete result["fields"]["animal"]["cache"];
->>>>>>> main
             }
           }
         }
       }
     } else {
-<<<<<<< HEAD
       if (has_animal) {
         if (animal_field.hasOwnProperty("cache")) {
           if (animal_field["cache"] == false) {
             delete animal_field["cache"];
-=======
-      if (result["fields"].hasOwnProperty("animal")) {
-        if (result["fields"]["animal"].hasOwnProperty("cache")) {
-          if (result["fields"]["animal"]["cache"] === false) {
-            delete result["fields"]["animal"]["cache"];
->>>>>>> main
           }
         }
       }
@@ -947,21 +927,12 @@ function deleteAnalyzer_400(testgroup, analyzerName){
           links: {}
         };
         meta.links[collectionName] = test["link"];
-<<<<<<< HEAD
         [[viewSVCache, true], /*[viewPSCache, true], [viewPKCache, true]*/, [viewNoCache, false]].forEach(viewTest => {
           let view = viewTest[0];
           if (view == undefined) {
             return;
           }
           view.properties(meta);
-=======
-        viewCache.properties(meta);
-        viewNoCache.properties(meta);
-
-        if (cacheSizeSupported) {
-          // Should we check that current link will use cache?
-          let utilizeCache = test["link"]["utilizeCache"];
->>>>>>> main
 
           if (cacheSizeSupported && isEnterprise) {
             let utilizeCache = test["link"]["utilizeCache"]; // is cache utilized by link?
@@ -1068,11 +1039,7 @@ function deleteAnalyzer_400(testgroup, analyzerName){
       } else {
         // current and previous versions are aware of 'cache'. 
         // Check that value is present and equal to value from previous version
-<<<<<<< HEAD
         if (viewSVCache.properties()["storedValues"][0]["cache"] != true) {
-=======
-        if (viewCache.properties()["storedValues"][0]["cache"] !== true) {
->>>>>>> main
           throw new Error("cache value for storedValues is not 'true'!");
         }
         // SHOULD BE UNCOMMENTED AFTER FIXING SEARCH-466
