dd5f3ab214f6beacdf874164fd2b724f74b6e820
Vitaly Pryakhin <vitaly@arangodb.com>
2023-05-31 20:47:41 +0300

    fix version check remove boilerplate comments and print statements

--- test_data/makedata_suites/005_check_system_collection.js
+++ test_data/makedata_suites/005_check_system_collection.js
@@ -4,21 +4,17 @@
   return {
     isSupported: function (version, oldVersion, options, enterprise, cluster) {
       // strip off -nightly etc:
-      //let ver = semver.parse(oldVersion.split('-')[0]);
+      let ver = semver.parse(oldVersion.split('-')[0]);
       return semver.gte(ver, "3.11.0");
     },
 
     makeDataDB: function (options, isCluster, isEnterprise, database, dbCount) {
-      // All items created must contain dbCount
-      print(`making per database data ${dbCount}`);
       return 0;
     },
     makeData: function (options, isCluster, isEnterprise, dbCount, loopCount) {
-      // All items created must contain dbCount and loopCount
-      print(`making data ${dbCount} ${loopCount}`);
+        return;
     },
     checkDataDB: function (options, isCluster, isEnterprise, database, dbCount, readOnly) {
-      // check per DB
       return 0;
     },
     checkData: function (options, isCluster, isEnterprise, dbCount, loopCount, readOnly) {
@@ -28,14 +24,12 @@
       const properties = db[pregelSystemCollectionName].properties();
       assertTrue(properties.isSystem);
       print(`Verified availability of system collection: ${pregelSystemCollectionName}`);
-      //print(`checking data ${dbCount} ${loopCount}`);
     },
     clearDataDB: function (options, isCluster, isEnterprise, database, dbCount) {
-      // check per DB
       return 0;
     },
     clearData: function (options, isCluster, isEnterprise, dbCount, loopCount) {
-      //print(`checking data ${dbCount} ${loopCount}`);
+      return;
     }
   };
 
