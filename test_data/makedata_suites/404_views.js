e7b675f681b0c1155d1bf9b5200dcc139bda0f8a
Vitaly Pryakhin <vitaly@arangodb.com>
2024-03-27 15:51:22 +0200

    enable running tests on a oneshard deployment

--- test_data/makedata_suites/404_views.js
+++ test_data/makedata_suites/404_views.js
@@ -47,8 +47,8 @@
         let collectionName0 = `collection_wand_0${dbCount}`;
         let collectionName1 = `collection_wand_1${dbCount}`;
 
-        let c0 = db._create(collectionName0, {"numberOfShards": 3, "replicationFactor": 2, "writeConcern": 1});
-        let c1 = db._create(collectionName1, {"numberOfShards": 3, "replicationFactor": 2, "writeConcern": 1});
+        let c0 = createCollectionSafe(collectionName0, 3, 2, {"writeConcern": 1})
+        let c1 = createCollectionSafe(collectionName1, 3, 2, {"writeConcern": 1})
 
         let meta0 = {
           'links': { },
