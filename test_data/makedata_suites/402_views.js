7d68a1f64c6357b7de5a204e3b05171044eb7944
Wilfried Goesgens <willi@arangodb.com>
2025-03-19 09:47:55 +0100

    only print name

--- test_data/makedata_suites/402_views.js
+++ test_data/makedata_suites/402_views.js
@@ -978,7 +978,7 @@ function deleteAnalyzer_400(testgroup, analyzerName){
           if (view === undefined) {
             return;
           }
-          print(`402: view in process: ${JSON.stringify(view)}`);
+          print(`402: view in process: ${JSON.stringify(view.name())}`);
           view.properties(meta);
 
           if (cacheSizeSupported && isEnterprise) {
