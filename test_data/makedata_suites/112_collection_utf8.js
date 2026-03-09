4471235df9e26ccc4366474bb80f5c6b96453fb8
Jure Bajic <jure.bajic94@gmail.com>
2026-03-09 14:25:17 +0100

    Fix 4.0 execution

--- test_data/makedata_suites/112_collection_utf8.js
+++ test_data/makedata_suites/112_collection_utf8.js
@@ -1,8 +1,10 @@
 /* global print, db, progress, createCollectionSafe, createIndexSafe, assertCollectionCount, assertIndexType, assertIndexCount, time, rand, semver, aql, runAqlQueryResultCount, writeData, resetRCount, getValue, isInstrumented */
 
 // This is the ArangoDB 4.0+ version of 102_collection_utf8.js
-// In 4.0, hash and skiplist indexes are converted to persistent.
-// makeData runs only on 4.0+ (not on 3.12). checkData runs after upgrade on 4.0 and verifies that indexes created by 102 (hash/skiplist) are now persistent.
+// Version roles: 102 makeData [current < 4.0] on 3.12 only; 102 checkData [current < 4.0] not run after upgrade.
+// 112 makeData [current >= 4.0] only on 4.0; 112 checkData [current >= 4.0] runs on 4.0 after upgrade.
+// So we can have makeData from 102 and checkData from 112 (upgrade: 3.12->4.0), or makeData and checkData from 112 (native 4.0 e.g. dump).
+// In 4.0, hash/skiplist from 102 are converted to persistent; 112 checkData verifies that and also accepts data created by 112 makeData (cpersistent_).
 
 (function () {
   let extendedNames = ["ᇤ፼ᢟ⚥㑸ন", "に楽しい新習慣", "うっとりとろける", "זַרקוֹר", "ስፖትላይት", "بقعة ضوء", "ուշադրության կենտրոնում", "🌸🌲🌵 🍃💔"];
@@ -82,55 +84,49 @@
       print(`${Date()} 112: using ${baseName}`);
     },
     checkData: function (options, isCluster, isEnterprise, dbCount, loopCount, readOnly) {
-      // Post-upgrade check: data was created by 102 on 3.12 (chash_, cskip_, etc.). Verify indexes are now persistent.
+      // 102 makeData + 112 checkData (upgrade): chash_, cskip_ present. 112 makeData + 112 checkData (native 4.0/dump): cpersistent_ present.
+      // Detect which schema we have and run the matching checks.
       print(`${Date()} 112: checking data ${dbCount} ${loopCount}`);
       print(`${Date()} 112: using ${baseName}`);
       db._useDatabase(baseName);
       let cnames = [];
       db._collections().forEach(col => { cnames.push(col.name());});
+      let hasCpersistent = cnames.indexOf(`cpersistent_${dbCount}${extendedNames[1]}`) !== -1;
+      let hasChashCskip = cnames.indexOf(`chash_${dbCount}${extendedNames[1]}`) !== -1 && cnames.indexOf(`cskip_${dbCount}${extendedNames[2]}`) !== -1;
+      let expectedCollections;
+      if (hasCpersistent && !hasChashCskip) {
+        expectedCollections = [`c_${dbCount}${extendedNames[0]}`, `cpersistent_${dbCount}${extendedNames[1]}`, `cfull_${dbCount}${extendedNames[3]}`, `cgeo_${dbCount}${extendedNames[4]}`, `cunique_${dbCount}${extendedNames[5]}`, `cmulti_${dbCount}${extendedNames[6]}`, `cempty_${dbCount}${extendedNames[7]}`];
+      } else {
+        expectedCollections = [`c_${dbCount}${extendedNames[0]}`, `chash_${dbCount}${extendedNames[1]}`, `cskip_${dbCount}${extendedNames[2]}`, `cfull_${dbCount}${extendedNames[3]}`, `cgeo_${dbCount}${extendedNames[4]}`, `cunique_${dbCount}${extendedNames[5]}`, `cmulti_${dbCount}${extendedNames[6]}`, `cempty_${dbCount}${extendedNames[7]}`];
+      }
       let allFound = true;
       let notFound = [];
-      [`c_${dbCount}${extendedNames[0]}`,
-       `chash_${dbCount}${extendedNames[1]}`,
-       `cskip_${dbCount}${extendedNames[2]}`,
-       `cfull_${dbCount}${extendedNames[3]}`,
-       `cgeo_${dbCount}${extendedNames[4]}`,
-       `cunique_${dbCount}${extendedNames[5]}`,
-       `cmulti_${dbCount}${extendedNames[6]}`,
-       `cempty_${dbCount}${extendedNames[7]}`].forEach(colname => {
-         let foundOne = false;
-         cnames.forEach(oneColName => {
-           if (oneColName === colname) {
-             foundOne = true;
-           }
-         });
-         if (!foundOne) {
-           print(`${Date()} 112: Didn't find this collection: ${colname}`);
-           notFound.push(colname);
-           allFound = false;
-         }
-       });
+      expectedCollections.forEach(colname => {
+        let foundOne = false;
+        cnames.forEach(oneColName => {
+          if (oneColName === colname) {
+            foundOne = true;
+          }
+        });
+        if (!foundOne) {
+          print(`${Date()} 112: Didn't find this collection: ${colname}`);
+          notFound.push(colname);
+          allFound = false;
+        }
+      });
       if (!allFound) {
         throw new Error(`not all collections were present on the system!: ${notFound} All collections: ${cnames}`);
       }
 
       let c = db._collection(`c_${dbCount}${extendedNames[0]}`);
-      let chash = db._collection(`chash_${dbCount}${extendedNames[1]}`);
-      let cskip = db._collection(`cskip_${dbCount}${extendedNames[2]}`);
       let cfull = db._collection(`cfull_${dbCount}${extendedNames[3]}`);
       let cgeo = db._collection(`cgeo_${dbCount}${extendedNames[4]}`);
       let cunique = db._collection(`cunique_${dbCount}${extendedNames[5]}`);
       let cmulti = db._collection(`cmulti_${dbCount}${extendedNames[6]}`);
       let cempty = db._collection(`cempty_${dbCount}${extendedNames[7]}`);
 
-      // Check indexes (hash/skiplist from 102 are now persistent in 4.0):
       progress("112: checking indices");
-
       assertIndexCount(c, 1);
-      assertIndexCount(chash, 2);
-      assertIndexType(chash, 1, 'persistent');
-      assertIndexCount(cskip, 2);
-      assertIndexType(cskip, 1, 'persistent');
       assertIndexCount(cfull, 1);
       assertIndexCount(cgeo, 2);
       assertIndexType(cgeo, 1, 'geo');
@@ -141,37 +137,56 @@
       assertIndexType(cmulti, 2, 'persistent');
       assertIndexCount(cempty, 1);
 
-      // Verify no fulltext indexes remain after upgrade to 4.0
-      [cfull, cmulti].forEach(col => {
-        col.getIndexes().forEach(idx => {
-          if (idx.type === 'fulltext') {
-            throw new Error(`112: fulltext index still exists on ${col.name()} after upgrade to 4.0!`);
-          }
+      if (hasCpersistent && !hasChashCskip) {
+        let cpersistent = db._collection(`cpersistent_${dbCount}${extendedNames[1]}`);
+        assertIndexCount(cpersistent, 2);
+        assertIndexType(cpersistent, 1, 'persistent');
+        assertCollectionCount(c, getValue(1000) * options.dataMultiplier);
+        assertCollectionCount(cpersistent, getValue(12345) * options.dataMultiplier);
+        assertCollectionCount(cgeo, getValue(5245) * options.dataMultiplier);
+        assertCollectionCount(cfull, getValue(6253) * options.dataMultiplier);
+        assertCollectionCount(cunique, getValue(5362) * options.dataMultiplier);
+        assertCollectionCount(cmulti, getValue(12346) * options.dataMultiplier);
+        progress("112: query 1");
+        let searchID = isInstrumented ? "id101" : "id1001";
+        runAqlQueryResultCount(aql`FOR x IN ${c} FILTER x.a == ${searchID} RETURN x`, 1);
+        progress("112: query 2");
+        searchID = isInstrumented ? "id105" : "id10452";
+        runAqlQueryResultCount(aql`FOR x IN ${cpersistent} FILTER x.a == ${searchID} RETURN x`, 1);
+      } else {
+        let chash = db._collection(`chash_${dbCount}${extendedNames[1]}`);
+        let cskip = db._collection(`cskip_${dbCount}${extendedNames[2]}`);
+        assertIndexCount(chash, 2);
+        assertIndexType(chash, 1, 'persistent');
+        assertIndexCount(cskip, 2);
+        assertIndexType(cskip, 1, 'persistent');
+        [cfull, cmulti].forEach(col => {
+          col.getIndexes().forEach(idx => {
+            if (idx.type === 'fulltext') {
+              throw new Error(`112: fulltext index still exists on ${col.name()} after upgrade to 4.0!`);
+            }
+          });
         });
-      });
+        if (cunique.getIndexes()[1].unique !== true) { throw new Error(`Mandarin ${cunique.getIndexes()[1].unique}`); }
+        assertCollectionCount(c, getValue(1000) * options.dataMultiplier);
+        assertCollectionCount(chash, getValue(12345) * options.dataMultiplier);
+        assertCollectionCount(cskip, getValue(2176) * options.dataMultiplier);
+        assertCollectionCount(cgeo, getValue(5245) * options.dataMultiplier);
+        assertCollectionCount(cfull, getValue(6253) * options.dataMultiplier);
+        assertCollectionCount(cunique, getValue(5362) * options.dataMultiplier);
+        assertCollectionCount(cmulti, getValue(12346) * options.dataMultiplier);
+        progress("112: query 1");
+        let searchID = isInstrumented ? "id101" : "id1001";
+        runAqlQueryResultCount(aql`FOR x IN ${c} FILTER x.a == ${searchID} RETURN x`, 1);
+        progress("112: query 2");
+        searchID = isInstrumented ? "id105" : "id10452";
+        runAqlQueryResultCount(aql`FOR x IN ${chash} FILTER x.a == ${searchID} RETURN x`, 1);
+        progress("112: query 3");
+        searchID = "id" + (isInstrumented ? 1339 : 13948 * options.dataMultiplier);
+        runAqlQueryResultCount(aql`FOR x IN ${cskip} FILTER x.a == ${searchID} RETURN x`, 1);
+      }
 
-      if (cunique.getIndexes()[1].unique !== true) { throw new Error(`Mandarin ${cunique.getIndexes()[1].unique}`); }
-
-      // Check data:
-      progress("112: checking counts");
-      assertCollectionCount(c, getValue(1000) * options.dataMultiplier);
-      assertCollectionCount(chash, getValue(12345) * options.dataMultiplier);
-      assertCollectionCount(cskip, getValue(2176) * options.dataMultiplier);
-      assertCollectionCount(cgeo, getValue(5245) * options.dataMultiplier);
-      assertCollectionCount(cfull, getValue(6253) * options.dataMultiplier);
-      assertCollectionCount(cunique, getValue(5362) * options.dataMultiplier);
-      assertCollectionCount(cmulti, getValue(12346) * options.dataMultiplier);
-
-      // Check a few queries (match 102's query logic):
-      progress("112: query 1");
-      let searchID = isInstrumented ? "id101" : "id1001";
-      runAqlQueryResultCount(aql`FOR x IN ${c} FILTER x.a == ${searchID} RETURN x`, 1);
-      progress("112: query 2");
-      searchID = isInstrumented ? "id105" : "id10452";
-      runAqlQueryResultCount(aql`FOR x IN ${chash} FILTER x.a == ${searchID} RETURN x`, 1);
-      progress("112: query 3");
-      searchID = "id" + (isInstrumented ? 1339 : 13948 * options.dataMultiplier);
-      runAqlQueryResultCount(aql`FOR x IN ${cskip} FILTER x.a == ${searchID} RETURN x`, 1);
+      let searchID;
       progress("112: query 4");
       runAqlQueryResultCount(aql`FOR x IN ${cempty} RETURN x`, 0);
       progress("112: query 5");
